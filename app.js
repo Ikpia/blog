ba54965f5021da9ad6e17f2541ac8a4b91a37a94
Ikpia <echukwuka97@gmail.com>
2022-09-16 12:03:54 +0100

    'commit'

--- app.js
+++ app.js
@@ -113,8 +113,11 @@ app.post("/compose", function (request, respond) {
 
 
 
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started Successfully");
 });
